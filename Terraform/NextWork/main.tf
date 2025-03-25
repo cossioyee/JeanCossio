3cca49305d31bd6128001d188e11bdd672e0a743
jean <jean@gmail.com>
2025-03-25 16:35:39 -0500

    Ajustando el tf para continuar con el proyecto del dia 2

--- Terraform/NextWork/main.tf
+++ Terraform/NextWork/main.tf
@@ -19,9 +19,32 @@ resource "aws_instance" "app_server" {
   vpc_security_group_ids = [
     "sg-0982f4b699100a2b5"
   ]
-
+  key_name = "myWebAppKeyPair"
   tags = {
     Name = "NextWork-WebApp"
   }
-}
 
+  provisioner "remote-exec" {
+    inline = [
+      "wget https://archive.apache.org/dist/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.tar.gz",
+      "sudo tar -xzf apache-maven-3.5.2-bin.tar.gz -C /opt",
+      "sudo dnf install -y java-1.8.0-amazon-corretto-devel",
+      "echo 'export JAVA_HOME=/usr/lib/jvm/java-1.8.0-amazon-corretto.x86_64' >> ~/.bashrc",
+      "echo 'export PATH=/usr/lib/jvm/java-1.8.0-amazon-corretto.x86_64/jre/bin/:/opt/apache-maven-3.5.2/bin:$PATH' >> ~/.bashrc",
+      "source ~/.bashrc",
+      "mvn -v",
+      "java -version",
+      "mvn archetype:generate -DgroupId=com.nextwork.app -DartifactId=nextwork-web-project -DarchetypeArtifactId=maven-archetype-webapp -DinteractiveMode=false",
+      "sudo dnf update -y",
+      "sudo dnf install -y git",
+      "git --version"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user" // Replace with the appropriate username for your AMI
+      private_key = file("~/.ssh/myWebAppKeyPair.pem") // Path to your private key
+      host        = self.public_ip
+    }
+  }
+}
