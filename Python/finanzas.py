7085b09165477fa6fc92b625cda37407033df03f
Jean Cossio <cossioyee16@yahoo.es>
2025-05-17 19:11:57 -0500

    agregando solo cambios en la finanzas

--- Python/finanzas.py
+++ Python/finanzas.py
@@ -56,6 +56,7 @@ df['Entrante'].fillna(0, inplace=True)
 df['Mes'] = pd.to_datetime(df['Fecha'], format='%d %B %Y').dt.month
 df['Año'] = pd.to_datetime(df['Fecha'], format='%d %B %Y').dt.year
 
+
 # Eliminar la columna "Monto"
 df.drop(columns=['Monto'], inplace=True)
 
@@ -63,7 +64,7 @@ df.drop(columns=['Monto'], inplace=True)
 saldo_total = df.pop('Saldo total')
 df['Saldo total'] = saldo_total
 
-# Insertar filas en cada cambio de mes
+# Insertar filas en cada cambio de mes y sumar Saliente y Entrante
 new_rows = []
 previous_month = None
 previous_year = None
@@ -100,6 +101,8 @@ ensa_fill = PatternFill(start_color='EAE888', end_color='EAE888', fill_type='sol
 visa_fill = PatternFill(start_color='B8B8B8', end_color='B8B8B8', fill_type='solid')
 seguro_fill = PatternFill(start_color='C41212', end_color='C41212', fill_type='solid')
 netflix_fill = PatternFill(start_color='8992E7', end_color='8992E7', fill_type='solid')
+anelys_fill = PatternFill(start_color='FF0000', end_color='FF0000', fill_type='solid')
+naranja_fill = PatternFill(start_color='FFA500', end_color='FFA500', fill_type='solid')
 
 # Obtener el índice de la columna "Descripcion"
 desc_col_index = 0
@@ -120,24 +123,33 @@ for row in ws.iter_rows(min_row=2):
         elif "BANCA MOVIL TRANSFERENCIA A 0301011162515 ASAMBLEA DE PROPIETARIOS DEL P.H. ALTATERRA" in row[desc_col_index - 1].value:
             for cell in row:
                 cell.fill = mante_fill
-        elif "BANCA MÓVIL TIGO HOGAR - PYME (94827659)" in row[desc_col_index - 1].value:
+        elif "94827659" in row[desc_col_index - 1].value: #Tigo
             for cell in row:
                     cell.fill = tigo_fill
-        elif "BANCA MÓVIL IDAAN (945168)" in row[desc_col_index - 1].value:
+        elif "945168" in row[desc_col_index - 1].value: #IDAAN
             for cell in row:
-                    cell.fill = idaan_fill
-        elif "BANCA MÓVIL ENSA (ELEKTRA NORESTE) (000021426294)" in row[desc_col_index - 1].value:
+                    cell.fill = tigo_fill
+        elif "000021426294" in row[desc_col_index - 1].value: #ENSA
             for cell in row:
-                    cell.fill = ensa_fill
+                    cell.fill = tigo_fill
+        elif "356099660000" in row[desc_col_index - 1].value: #Data
+            for cell in row:
+                    cell.fill = tigo_fill
         elif "BANCA MOVIL PAGO VISA 4931-23XX-XXXX-9827 JEAN CARLOS COSSIO YEE" in row[desc_col_index - 1].value:
             for cell in row:
                     cell.fill = visa_fill
-        elif "BANCA EN LINEA CIA INTERNACIONAL DE SEGU (000000000300442612)" in row[desc_col_index - 1].value:
+        elif "000000000300442612" in row[desc_col_index - 1].value:
             for cell in row:
                     cell.fill = seguro_fill
         elif "BANCA EN LINEA TRANSFERENCIA A 0472997148051 LUIS ALBERTO GONZALEZ REAL nefli" in row[desc_col_index - 1].value:
             for cell in row:
                     cell.fill = netflix_fill
+        elif "METRO Y METROBUS " in row[desc_col_index - 1].value:
+            for cell in row:
+                    cell.fill = naranja_fill
+        elif "BANCA EN LINEA TRANSFERENCIA DE ANELYS KRISCHELL GONZALEZ CHAVEZ" in row[desc_col_index - 1].value:
+            for cell in row:
+                    cell.fill = anelys_fill
 
 
 # for row in ws.iter_rows(min_row=2):
@@ -173,151 +185,3 @@ for row in ws.iter_rows(min_row=2):
 # Guardar los cambios en el archivo Excel
 wb.save(new_file_path)
 
-# # Filtrar las filas que contienen "Stella" en la columna "Area Responsable"
-# df_filtered = df[df['Area Responsable'].str.contains('Stella', na=False)].copy()
-
-# # Convertir la columna "Computer Name" a minúsculas
-# df_filtered.loc[:, 'Computer Name'] = df_filtered['Computer Name'].str.lower()
-
-# # Clasificar la columna "Computer Name"
-# def classify_computer_name(name):
-#     if name.startswith('bgcd'):
-#         return 'dev'
-#     elif name.startswith('bgcp'):
-#         return 'amb'
-#     elif name.startswith('bgca'):
-#         return 'dr'
-#     elif name.startswith('bgdc'):
-#         return 'prod'
-#     else:
-#         return 'unknown'
-
-# df_filtered.loc[:, 'Classification'] = df_filtered['Computer Name'].apply(classify_computer_name)
-
-# # Clasificar la columna "Computer Name" por tecnología
-# def classify_tech(name):
-#     if 'ntps' in name:
-#         return 'ntp'
-#     if 'dynatrace' in name:
-#         return 'dynatrace'
-#     if 'nortexdc' in name:
-#         return 'decision center'
-#     if 'dsg' in name:
-#         return 'designer'
-#     if 'log' in name:
-#         return 'ELK'
-#     if 'lic' in name:
-#         return 'designer licencia'
-#     if 'hpv' in name or 'hyper' in name:
-#         return 'hypervisor'
-#     if 'rh0' in name:
-#         return 'hosts'
-#     if 'mon' in name:
-#         return 'monitoreo'
-#     if 'odmrs' in name:
-#         return 'odm rs'
-#     if 'odmbd' in name:
-#         return 'odm db'
-#     if 'xace' in name:
-#         return 'ace'
-#     if 'ecrionap' in name:
-#         return 'ecrion ap'
-#     if 'ecriondb' in name or 'ecrionbd' in name:
-#         return 'ecrion db'
-#     if 'engagelb' in name:
-#         return 'engage lb'
-#     if 'engagedb' in name or 'engdb' in name:
-#         return 'engage db'
-#     if 'engageap' in name or 'engap' in name or 'engagecap' in name:
-#         return 'engage ap'
-#     if 'lb' in name or 'ngx' in name:
-#         return 'nginx'
-#     if 'cg' in name or 'cgen' in name:
-#         return 'jboss / tomcat'
-#     if 'xels' in name or 'xes' in name or 'aes' in name:
-#         return 'elasticsearch'
-#     if 'camdb' in name or 'db' in name:
-#         return 'db'
-#     if 'api' in name or 'apr' in name:
-#         return 'container'
-#     if 'ap' in name:
-#         return 'ap liferay'
-#     else:
-#         return 'unknown'
-
-# df_filtered.loc[:, 'Tech'] = df_filtered['Computer Name'].apply(classify_tech)
-
-# # Convertir la columna "Last Patched Time" a formato fecha (día, mes y año con el mes en palabras)
-# df_filtered['Last Patched Time'] = pd.to_datetime(df_filtered['Last Patched Time'], errors='coerce').dt.strftime('%Y-%m-%d')
-# df_filtered['Last Boot Time'] = pd.to_datetime(df_filtered['Last Boot Time'], errors='coerce').dt.strftime('%Y-%m-%d')
-# df_filtered['Last Patched Month'] = pd.to_datetime(df_filtered['Last Patched Time'], errors='coerce').dt.strftime('%B')
-# df_filtered['Last Boot Month'] = pd.to_datetime(df_filtered['Last Boot Time'], errors='coerce').dt.strftime('%B')
-
-# # Eliminar las columnas especificadas
-# columns_to_drop = ['Area Responsable', 'Last Successful Scan', 'Remote Office', 'Area Responsable AP']
-# df_filtered.drop(columns=columns_to_drop, inplace=True)
-
-# # Guardar el DataFrame filtrado en una nueva hoja llamada "Stella"
-# with pd.ExcelWriter(new_file_path, mode='a', engine='openpyxl') as writer:
-#     df_filtered.to_excel(writer, sheet_name='Stella', index=False)
-
-# # Cargar el archivo Excel con openpyxl
-# wb = load_workbook(new_file_path)
-# ws = wb['Stella']
-
-# # Obtener el mes actual
-# current_month = pd.Timestamp.now().strftime('%B')
-# previous_month = (pd.Timestamp.now() - pd.DateOffset(months=1)).strftime('%B')
-
-# # Definir los colores
-# green_fill = PatternFill(start_color='00FF00', end_color='00FF00', fill_type='solid')
-# red_fill = PatternFill(start_color='FF0000', end_color='FF0000', fill_type='solid')
-# yellow_fill = PatternFill(start_color='FFFF00', end_color='FFFF00', fill_type='solid')
-
-# # Obtener el índice de la columna "Last Patched Month"
-# lpm_col_index = None
-# for idx, cell in enumerate(ws[1]):
-#     if cell.value == 'Last Patched Month':
-#         lpm_col_index = idx + 1
-#         break
-
-# # Aplicar el color a la columna "Last Patched Month"
-# for row in ws.iter_rows(min_row=2, min_col=lpm_col_index, max_col=lpm_col_index):
-#     for cell in row:
-#         if cell.value == current_month:
-#             cell.fill = green_fill
-#         elif cell.value == previous_month:
-#             cell.fill = yellow_fill
-#         else:
-#             cell.fill = red_fill
-
-# # Aplicar el color a la columna "Last Boot Month"
-# for row in ws.iter_rows(min_row=2, min_col=ws.max_column, max_col=ws.max_column):
-#     for cell in row:
-#         if cell.value == current_month:
-#             cell.fill = green_fill
-#         elif cell.value == previous_month:
-#             cell.fill = yellow_fill
-#         else:
-#             cell.fill = red_fill
-
-# # Obtener el índice de la columna "Health"
-# health_col_index = None
-# for idx, cell in enumerate(ws[1]):
-#     if cell.value == 'Health':
-#         health_col_index = idx + 1
-#         break
-
-# # Aplicar el color a la columna "Health"
-# if health_col_index:
-#     for row in ws.iter_rows(min_row=2, min_col=health_col_index, max_col=health_col_index):
-#         for cell in row:
-#             if cell.value == 'Healthy':
-#                 cell.fill = green_fill
-#             elif cell.value == 'Vulnerable':
-#                 cell.fill = yellow_fill
-#             elif cell.value == 'Highly Vulnerable':
-#                 cell.fill = red_fill
-
-# # Guardar los cambios en el archivo Excel
-# wb.save(new_file_path)
