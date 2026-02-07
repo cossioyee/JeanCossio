2970f23287f4c712a327a61d96de90f8cca81404
Jean Cossio <cossioyee16@yahoo.es>
2026-02-07 12:15:09 -0500

    practicando crear VPC con TF

--- Terraform/Personal/vpc-tf-jean/main.tf
+++ Terraform/Personal/vpc-tf-jean/main.tf
@@ -15,3 +15,45 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+#-------------------------------------------------------------------
+# Variables
+#-------------------------------------------------------------------
+variable "environment" {
+  description = "Nombre del ambiente"
+  type        = string
+  default     = "dev"
+}
+
+variable "vpc_cidr" {
+  description = "Rango de IPs para la VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDRs para subnets publicas"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDRs para subnets privadas"
+  type        = list(string)
+  default     = ["10.0.10.0/24", "10.0.20.0/24"]
+}
+
+#-------------------------------------------------------------------
+# VPC Principal
+#-------------------------------------------------------------------
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name        = "${var.environment}-vpc"
+    Environment = var.environment
+  }
+}
+
